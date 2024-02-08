955cd86d2331b28b00975478c9f9f14909c33e9a
Kikingod <156136292+Kikingod@users.noreply.github.com>
2024-02-08 21:13:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Before pause');
+
+// Using setTimeout to create a delay of 1000 milliseconds (1 second)
+setTimeout(function() {
+  console.log('After one second pause');
+  // The code inside this function will be executed after the delay
+}, 1000);
+
+console.log('After setTimeout, but before pause');
---
+++ index.py
@@ -0,0 +1,3 @@
+for x in range(3):
+    for i in range(x):
+        print
